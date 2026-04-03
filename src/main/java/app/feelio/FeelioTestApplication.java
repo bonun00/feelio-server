c6d3c4c95f38a5d35815fdf0abeaa755e07dbed9
bonun00 <lifesciences00@naver.com>
2026-04-03 15:05:47 +0900

    fix: 배포 환경에서 .env 파일 부재로 인한 실행 오류 수정

--- src/main/java/app/feelio/FeelioTestApplication.java
+++ src/main/java/app/feelio/FeelioTestApplication.java
@@ -11,7 +11,7 @@ public class FeelioTestApplication {
 
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().load();
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 		dotenv.entries().forEach(entry ->
 			System.setProperty(entry.getKey(), entry.getValue())
 		);
